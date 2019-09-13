fa527e4406704c658ee24a22a64ef1984580c316
Dusty <dustygaron@hotmail.com>
2019-09-13 12:51:56 -0400

    Styled the gallery modals.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -27,22 +27,9 @@ $(document).ready(function () {
 })
 
 // ------------------
-// MAGNIFIC
+// TOOLTIP
 // ------------------
-// $('.test-popup-link').magnificPopup({
-//   type: 'image',
-//   gallery: { enabled: true },
-//   type: 'image',
-//   image: {
-//     titleSrc: 'title'
-//     // this tells the script which attribute has your caption
-//   }
-// })
-
-// // This will create a single gallery from all elements that have class "gallery-item"
-// $('.gallery-item').magnificPopup({
-//   type: 'image',
-//   gallery: {
-//     enabled: true
-//   }
-// })
+//   document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.tooltipped');
+var instances = M.Tooltip.init(elems, options);
+  });
